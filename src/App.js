31313e69fe3ce4e0796feee0663540aee9e7bf84
Govind <agovindchavanb@gmail.com>
2023-06-07 10:24:54 +0530

    api fetching

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Loader from './Loader';
 
-function App() {
+const App = () => {
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://dummyjson.com/products');
+        const data = await response.json();
+        console.log(data); // Print the response to the console
+        if (Array.isArray(data.products)) {
+          setProducts(data.products);
+        }
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 className="heading">Product List</h1>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <ul className="product-grid">
+          {products.map((product) => (
+            <li key={product.id} className="product-item">
+              <h3 className="product-title">{product.title}</h3>
+              <p className="product-description">{product.description}</p>
+              <p className="product-price">Price: ${product.price}</p>
+              <p className="product-discount">Discount: {product.discountPercentage}%</p>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
-}
+};
 
 export default App;
